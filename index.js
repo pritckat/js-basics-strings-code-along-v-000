d01ee3163516857fd0f867659bb55968b9d31680
Katrina Pritchard <pritckat@gmail.com>
2018-10-10 22:28:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let currentUser = "Katrina";
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 let excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser.toUpperCase() + "!";
+let shortGreeting = "Welcome, "
